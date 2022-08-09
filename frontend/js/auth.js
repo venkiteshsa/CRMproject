d4b032aa9a4afbb1c45016353ebab22b16b7012b
venkiteshsa <venkitesh.sa@inapp.com>
2022-08-09 12:29:49 +0530

    admin auth pages added

---
+++ frontend/js/auth.js
@@ -0,0 +1,12 @@
+function Verify() {
+    var userRef = "SuperBob";
+    var passRef = "12RobotoLove";
+    
+    var user =  document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    if (user == userRef || pass == passRef) {
+    alert("Connexion")
+    } else {
+    alert("It's seem you make a mistake...")
+    }
+  };
