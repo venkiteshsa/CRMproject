f757a6f628ee3c5dcc5b4c02fae0106c9a38a8de
anupamb-2000 <anupam.b@inapp.com>
2022-08-18 23:23:20 +0530

    Updated use cases with material design

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,8 +1,15 @@
+// Sidebar Trigger
 document.addEventListener("DOMContentLoaded", function () {
     const options = {
         "edge": "left"
-    } 
+    }
 
     var elems = document.querySelectorAll(".sidenav");
     var instances = M.Sidenav.init(elems, options);
 });
+
+function clearFilters() {
+    document.getElementsByName("checkbox").forEach(element => {
+        element.checked = false;
+    });
+}
